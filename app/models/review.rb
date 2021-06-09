b05f598eecf53b1d194d92a812320ef09c9f80d1
Maxence Laplante <maxence.laplante@gmail.com>
2021-06-09 16:55:28 +0200

    Rake pas à 100%

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-end
+  validates :content, presence: true
+  validates_numericality_of :rating, only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+puts "Restaurant Deleted"
+
+sogood = { name: "So Good", address: "165 avenue de Bretagne, Lille", phone_number: "07 99 99 99 99", category: "french" }
+pausedej =  { name: "Pause Dej", address: "166 avenue de Bretagne, Lille", phone_number: "07 99 99 99 99", category: "french" }
+lafamille = { name: "La Famille", address: "167 avenue de Bretagne, Lille", phone_number: "07 99 99 99 99", category: "french" } 
+pekopeko = { name: "Peko Peko", address: "168 avenue de Bretagne, Lille", phone_number: "07 99 99 99 99", category: "japanese" } 
+bagel = { name: "Bagel", address: "169 avenue de Bretagne, Lille", phone_number: "07 99 99 99 99", category: "belgian" } 
+
+[ sogood, pausedej, lafamille, pekopeko, bagel ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "5 restaurants created"
+end
